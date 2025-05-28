6f6cf176a9b13da27734af1f3f8a6ede51d4bf7d
IvelinDankov <velin80@web.de>
2025-05-28 13:50:57 +0300

    making cvm schema and adding home view and layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
+import handlebars from "express-handlebars";
+import routes from "./routes.js";
 
 const app = express();
 
 const port = 3015;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to expresss.....");
-  res.end();
-});
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.use(routes);
 
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
